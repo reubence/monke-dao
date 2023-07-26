fd79008f3569d6ae42355222e77f1e1f93716730
Reuben Rapose <reuben.rapose@gmail.com>
2023-07-26 15:01:17 +0530

    first-commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import { Button, buttonVariants } from '@/components/ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -8,6 +10,9 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+        <Link href="https://google.com" className={buttonVariants({variant:'outline'})}>
+          Button
+        </Link>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
