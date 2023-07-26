45346307900ef0727bfe02dd473784b097d970b5
Reuben Rapose <reuben.rapose@gmail.com>
2023-07-26 21:49:29 +0530

    Header ✅

--- app/page.tsx
+++ app/page.tsx
@@ -25,9 +25,9 @@ export default function Home() {
     <div className="h-screen w-screen overflow-clip">
       <NavigationMenu />
       <main className="h-full w-full">
-        <div className="max-w-screen-7xl w-full rounded-[60px] overflow-x-clip">
+        <div className="max-w-screen-7xl w-full overflow-x-clip px-4 sm:px-8 lg:px-10 pt-20">
           <section className="flex flex-col w-full lg:flex-row items-stretch">
-            <div className="flex flex-col items-end lg:w-1/2">
+            <div className="flex flex-col lg:items-end lg:w-1/2">
               <Image
                 src={"/../public/nft-1.png"}
                 alt="Logo"
@@ -35,7 +35,7 @@ export default function Home() {
                 width={550}
               />
             </div>
-            <div className="lg:col-span-1 bg-chocolate h-full w-full justify-center lg:w-1/2">
+            <div className="lg:col-span-1 bg-white min-h-full justify-center lg:w-[663px]">
               <h1>SMB #1232</h1>
             </div>
           </section>
